673fc38f264a984363e3617535eaef3711fa150a
Alexey Tiholasov <alexey.tiholasov@apstecsystems.com>
2023-02-28 18:04:32 +0300

    small redesign

--- Tasks/statFromServerLog.py
+++ Tasks/statFromServerLog.py
@@ -7,22 +7,22 @@ def main():
     import pandas as pd
     import numpy as np
     import matplotlib.pyplot as plt
+    import matplotlib.ticker as ticker
     import seaborn as sns
     from datetime import datetime, timedelta
 
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help="Name for input file")
     parser.add_argument('-out', '--output_file', default="stat.сsv", help="Name for output file. Default = stat.csv")
-    parser.add_argument('-time', default=3600, help="Time delta in sec. Default = 3600 sec", type=int)
-    parser.add_argument('-ratio', default=0.85, help="Argument for the graph of the ratio of "
-                                                     "targets with an alarm to their total number. Default = 0.85", type=float)
-    parser.add_argument('-min_ghost_time', default=1, help="Minimum target lifetime to avoid"
-                                                           " becoming a ghost. Default = 1 sec", type=float)
+    parser.add_argument('-period', default=3600, help="Period for splitting statistics over time. Default = 3600 sec",
+                        type=int)
+    parser.add_argument('-ghost_cutoff_time', default=1,
+                        help="Minimum target lifetime to avoid becoming a ghost. Default = 1 sec", type=float)
     args = parser.parse_args()
 
     inp_file_name = args.input_file
     out_file_name = args.output_file
-    time_step_sec = args.time
+    time_step_sec = args.period
 
     log_file = open(inp_file_name, "r")
     str_data = log_file.read()
@@ -49,8 +49,8 @@ def main():
                     and int(arr_string[8]) == 1:
                 shift = data_for_NewTargets[2][N_words_target-2]
 
-            data_for_NewTargets[2].append(int(arr_string[8])+shift)
-            data_for_NewTargets[1].update({int(arr_string[8])+shift: 0})
+            data_for_NewTargets[2].append(int(arr_string[8]) + shift)
+            data_for_NewTargets[1].update({int(arr_string[8]) + shift: 0})
 
         if lines_of_file[i].find("alarm=") != -1:
             arr_string = lines_of_file[i].split()
@@ -68,14 +68,14 @@ def main():
 
         if lines_of_file[i].find("Deleted target") != -1:
             arr_string = lines_of_file[i].split()
-            if float(arr_string[16]) < args.min_ghost_time:
+            if float(arr_string[16]) < args.ghost_cutoff_time:
                 del_index = int(arr_string[8])+shift
                 data_for_NewTargets[1].update({del_index: 2})
 
     if len(data_for_NewTargets[0]) == 0:
         print("NO TARGETS LOG FILE")
     else:
-        first_line_for_output = ["Start time", "N_targets", "Targets_with_alarm", "Ratio"]
+        first_line_for_output = ["Start time", "N_targets", "Targets_with_alarm", "Alarm ratio"]
         dict_for_index_in_line = {}
         for type_anomal in all_alarms_types:
             first_line_for_output.append(type_anomal)
@@ -125,7 +125,7 @@ def main():
             if N_targets_no_ghost != 0:
                 ratio = float('{:.3f}'.format(N_targets_with_anomal / N_targets_no_ghost))  # 3 numbers after dot
             elif N_targets_with_anomal == 0:
-                ratio = 1.0
+                ratio = 0.0
             else:
                 ratio = -1.0  # check suddenly at 0 targets there are not 0 targets with an alarm
             out_intermediate_list.append(ratio)
@@ -153,9 +153,11 @@ def main():
             print("Output file format should be xlsx or csv")
 
         # Plotting
+        # Plotting first N_targets(period)
 
         width_for_plot = 1
-        level_of_good_result = args.ratio
+        plt.rcParams["figure.autolayout"] = True
+        plt.rcParams["figure.figsize"] = [15, 6]
         plt.figure('Log file statistics')
 
         x_axis = []     # Making only time on the axis x
@@ -164,33 +166,51 @@ def main():
         x_axis = np.array(x_axis)
         DF_out.index = x_axis
 
-        plt.subplot(1, 2, 1)
+        ax = plt.subplot(1, 2, 1)
         plt.title("Number of targets and targets with alarm")
         plt.xlabel("Time")
-        plt.xticks(np.arange(len(x_axis)), x_axis, rotation=45)
         plt.ylabel("Numbers")
+        ax.xaxis.set_tick_params(which='minor', bottom=False)
+        plt.xticks(np.arange(len(x_axis)), x_axis, rotation=45)
+        plt.minorticks_on()
+        plt.grid(which='major', color="gray")
+        plt.grid(which='minor', linestyle='dotted', axis="y", color="gray")
         plt.bar(np.arange(len(x_axis)), DF_out.iloc[:, 0], label="Full number of targets", align='edge',
-                width=width_for_plot / 2)
+                width=width_for_plot / 2, edgecolor="gray")
         plt.bar(np.arange(len(x_axis)) + width_for_plot / 2, DF_out.iloc[:, 1], label="Targets with alarm",
-                align='edge',
-                width=width_for_plot / 2)
+                align='edge', width=width_for_plot / 2, edgecolor="gray")
         plt.legend()
 
-        plt.subplot(1, 2, 2)
+        # Plotting second Alarm_ratio(period)
+
+        ax = plt.subplot(1, 2, 2)
         plt.title("Percentage of alarms")
         plt.xlabel("Time")
-        plt.xticks(rotation=45)
-        plt.ylabel("Ratio")
-        plt.step(x_axis, DF_out.iloc[:, 2], label="Ratio")
-        plt.plot(x_axis, np.ones(len(DF_out)) * level_of_good_result, "r--",
-                 label=f"Good result, {level_of_good_result} percent")
+        plt.ylabel("Alarm ratio")
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+        ax.xaxis.set_tick_params(which="minor", bottom=False, color="gray")
+        plt.xticks(np.arange(len(x_axis)), x_axis, rotation=45)
+        plt.grid(which='major')
+        plt.ylim([0., 1.])
+        plt.minorticks_on()
+        plt.bar(np.arange(len(x_axis)), DF_out.iloc[:, 2], label="Ratio", align='edge', width=width_for_plot,
+                color="#ffa600", edgecolor="gray")
         plt.legend()
 
-        DF_out_by_types = DF_out.iloc[:, 4:]
+        # Plotting third Alarm_types(period)
 
+        plt.rcParams["figure.autolayout"] = True
+        plt.rcParams["figure.figsize"] = [15, 6]
+        DF_out_by_types = DF_out.iloc[:, 3:]
+        DF_out_by_types = DF_out_by_types.astype(np.int)
+        arr_for_Transpose = np.array(DF_out_by_types)
+        arr_for_plot_alarm_types = np.transpose(arr_for_Transpose)
+        DF_out_by_types_right_order = pd.DataFrame(arr_for_plot_alarm_types, index=DF_out_by_types.columns,
+                                                   columns=DF_out_by_types.index)
         plt.figure('Log file statistics by types')
-        sns.heatmap(DF_out_by_types, annot=True, linewidth=.5)
+        sns.heatmap(DF_out_by_types_right_order, annot=True, linewidth=.5, fmt='g')
         plt.xticks(rotation=45)
+        plt.yticks(rotation=45)
         plt.show()
 
 
